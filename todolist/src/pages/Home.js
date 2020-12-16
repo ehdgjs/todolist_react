3c0d7a0b90171c9cb164261af6d0fdec81175b72
ehdgjs <zone7907@gmail.com>
2020-12-17 01:23:33 +0900

    조건 렌더 완료

--- todolist/src/pages/Home.js
+++ todolist/src/pages/Home.js
@@ -1,34 +1,30 @@
 import React from 'react'
 import $ from 'jquery'
 
-const Home = () => {
-	const setName = () => {
-		let name = $("#name").val();
-		localStorage.setItem('name', name);
-		window.location.reload();
-	}
-	//조건부 렌더링 진행중
-	const existName = () => {
-		if(localStorage.getItem('name') === null){
-			$('#div_name').show();
-		}else{
-			$('#div_name').hide();
-		}
-	}
-
-	return (
-		<div>
-			<h2>Home</h2>
-			<div id="div_name">
-				<input type="text" id="name" placeholder="이름을 입력하세요"/>
-				<input type="button" value="등록" onClick={() => setName()}/>
-			</div>
-		</div>
-	)
+const setName = () => {
+	let name = $("#name").val();
+	localStorage.setItem('name', name);
+	window.location.reload();
 }
 
 const Inputname = () => {
-	
+	if(localStorage.getItem('name') === null){
+		return  <div id="div_name">
+							<input type="text" id="name" placeholder="이름을 입력하세요"/>
+							<input type="button" value="등록" onClick={() => setName()}/>
+						</div>
+	}else{
+		return null;
+	}
+}
+
+const Home = () => {
+	return (
+		<div>
+			<h2>Home</h2>
+			<Inputname/>
+		</div>
+	)
 }
 
 export default Home;
