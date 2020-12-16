0ae6f52d957510d27e7e430c7928da35572e2948
ehdgjs <zone7907@gmail.com>
2020-12-17 01:00:14 +0900

    home 수정

--- todolist/src/pages/Home.js
+++ todolist/src/pages/Home.js
@@ -2,33 +2,33 @@ import React from 'react'
 import $ from 'jquery'
 
 const Home = () => {
-    const setName = () => {
-        let name = $("#name").val();
-        localStorage.setItem('name', name);
-        window.location.reload();
-    }
-    //조건부 렌더링 진행중
-    const existName = () => {
-        if(localStorage.getItem('name') === null){
-            $('#div_name').show();
-        }else{
-            $('#div_name').hide();
-        }
-    }
+	const setName = () => {
+		let name = $("#name").val();
+		localStorage.setItem('name', name);
+		window.location.reload();
+	}
+	//조건부 렌더링 진행중
+	const existName = () => {
+		if(localStorage.getItem('name') === null){
+			$('#div_name').show();
+		}else{
+			$('#div_name').hide();
+		}
+	}
 
-    return (
-        <div>
-            <h2>Home</h2>
-            <div id="div_name">
-                <input type="text" id="name" placeholder="이름을 입력하세요"/>
-                <input type="button" value="등록" onClick={() => setName()}/>
-            </div>
-        </div>
-    )
+	return (
+		<div>
+			<h2>Home</h2>
+			<div id="div_name">
+				<input type="text" id="name" placeholder="이름을 입력하세요"/>
+				<input type="button" value="등록" onClick={() => setName()}/>
+			</div>
+		</div>
+	)
 }
 
 const Inputname = () => {
-    
+	
 }
 
 export default Home;
